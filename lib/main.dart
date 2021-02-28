d9bf9c68da4b2a355a9977dee1a3498cb0c90372
David Pescariu <davidpescariu12@gmail.com>
2021-02-28 10:09:00 +0200

    Updated copyright year

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 /// This is the main entry point for the prisma.ai App, formerly known as 
 /// Safe Signal App - we are currently undergoing a rebranding.
 /// 
-/// (C) prisma.ai 2020 or respective owners of libraries
+/// (C) prisma.ai 2021 or respective owners of libraries
 ///
 /// * Public Source Release - Released 11 Nov 2020 | Updated Feb 2021 * 
 ///
